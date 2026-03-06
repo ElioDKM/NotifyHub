465ea2c892cb6f02a3dad81b62df4417074fdac4
ElioDK <eliodikoume@gmail.com>
2026-03-06 14:52:50 +0100

    fix: reorder ESLint rules for better organization and clarity

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -28,10 +28,16 @@ export default tseslint.config(
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
+
+      // ✅ Les 3 qui cassent souvent en CI
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-return': 'off',
       '@typescript-eslint/no-unsafe-argument': 'warn',
-      "prettier/prettier": ["error", { endOfLine: "auto" }],
+
       '@typescript-eslint/no-unsafe-call': 'off',
       '@typescript-eslint/no-unsafe-member-access': 'off',
+
+      'prettier/prettier': ['error', { endOfLine: 'auto' }],
     },
   },
 );
